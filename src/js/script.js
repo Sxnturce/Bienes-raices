f3a92369c664d1953a91b88b5f0c43db6271438d
SebastianSR <102806578+Sxnturce@users.noreply.github.com>
2024-04-10 15:27:37 -0500

    body with dinamic Overflow

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 const checkbox = document.getElementById("checkbox")
 const nav_links = document.querySelector(".nav__links")
-
+const body = document.querySelector("body")
 
 checkbox.addEventListener("click", activeNav)
 
@@ -8,9 +8,10 @@ checkbox.addEventListener("click", activeNav)
 function activeNav() {
     if (checkbox.checked) {
         nav_links.style.transform = "translateX(0%)"
-
+        body.classList.add("no-scroll")
     }
     else {
         nav_links.style.transform = ""
+        body.classList.remove("no-scroll")
     }
 }
