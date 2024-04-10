aeeb3873ade72b0d5cdaead2ec8cb3fce50aecf2
SebastianSR <102806578+Sxnturce@users.noreply.github.com>
2024-04-10 18:45:44 -0500

    Se agrego el apartado de Sobre nosotros

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,36 @@
-const checkbox = document.getElementById("checkbox")
-const nav_links = document.querySelector(".nav__links")
-const body = document.querySelector("body")
+window.addEventListener("DOMContentLoaded", () => {
 
-checkbox.addEventListener("click", activeNav)
+    const checkbox = document.getElementById("checkbox")
+    const nav_links = document.querySelector(".nav__links")
+    const body = document.querySelector("body")
+
+    //Eventos
+    checkbox.addEventListener("click", activeNav)
+    window.addEventListener("scroll", scrolled)
 
 
-function activeNav() {
-    if (checkbox.checked) {
-        nav_links.style.transform = "translateX(0%)"
-        body.classList.add("no-scroll")
+
+    //Funciones
+    function activeNav() {
+        if (checkbox.checked) {
+            nav_links.style.transform = "translateX(0%)"
+            body.classList.add("no-scroll")
+        }
+        else {
+            nav_links.style.transform = ""
+            body.classList.remove("no-scroll")
+        }
     }
-    else {
-        nav_links.style.transform = ""
-        body.classList.remove("no-scroll")
+
+
+    function scrolled() {
+        const header = document.querySelector(".container-header")
+        const scrollposition = window.scrollY
+        if (scrollposition > 100) {
+            header.classList.add("header_color")
+        }
+        else {
+            header.classList.remove("header_color")
+        }
     }
-}
+})
